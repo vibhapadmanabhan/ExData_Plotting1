207ae902f57ecb689ed9683097742e50d176117c
Vibha Padmanabhan <57306517+vibhapadmanabhan@users.noreply.github.com>
2020-09-27 23:16:13 +0530

    Added comments

--- plot1.R
+++ plot1.R
@@ -1,7 +1,15 @@
+#read data
 data<-read.table('household_power_consumption.txt', na.strings='?', header
                  =TRUE, sep=';')
+
+#subset relevant data
 data$Date<-as.Date(strptime(data$Date, format='%d/%m/%Y'))
 newData<-subset(data, Date==as.Date('2007-02-01') | Date==as.Date('2007-02-02'))
+
+#construct plot
 hist(newData$Global_active_power, col='red', main='Global Active Power', 
      xlab="Global Active Power (kilowatts)", ylab='Frequency')
+
+#save plot
 dev.copy(png, file='plot1.png')
+dev.off()
